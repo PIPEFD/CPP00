c390314012bf7761a337f054b7fdf2f2e941946d
David Felipe Bonilla Ladino <dbonilla@c3r1s6.42urduliz.com>
2024-11-04 18:55:45 +0100

    megaphone with ñ

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -12,31 +12,53 @@
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstring> // Para std::strlen
+#include <bitset>
 
+void process_arguments(int argc, char **str) {
+    int i = 0;
+    int j = 0;
+    while (str[i][j]) {
+        unsigned char current_char = (unsigned char)str[i][j];
 
-
-void process_arguments(int argc, char **str)
-{
-    int i = 1;
-    while (i  < argc)
-    {
-        int j = 0;
-        while(str[i][j])
-        {
-            std::cout << (char)std::toupper(str[i][j]);
+        if (current_char == 0xC3 && (unsigned char)str[i][j + 1] == 0xB1) {
+            std::cout << "\xC3\x91";
+            j += 2;
+        }
+        else if (current_char >= 'a' && current_char <= 'z') {
+            std::cout << (char)(current_char - ('a' - 'A'));
             j++;
         }
-if ()
-        i++;
+        else {
+            std::cout << current_char;
+            j++;
+        }
+    i++;
     }
     std::cout << std::endl;
 }
 
-int main (int argc, char **argv)
-{
+void printBits(const unsigned char* str) {
+    int i = 0;
+    while (str[i] != '\0') {
+        std::cout << "Byte " << i + 1 << ": " << std::bitset<8>(str[i]) << std::endl;
+        i++;
+    }
+}
+int main(int argc, char **argv) {
+
     if (argc > 1)
-         process_arguments(argc, argv);
+    {
+        printBits(reinterpret_cast<const unsigned char*>(argv[1]));
+        process_arguments(argc, argv);
+        printBits(reinterpret_cast<const unsigned char*>(argv[1]));
+
+    }
     else
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    return(0);
+    return 0;
 }
+
+
+
+
--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -3,33 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   Contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pipe <pipe@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: dbonilla <dbonilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 01:07:50 by pipe              #+#    #+#             */
-/*   Updated: 2024/10/18 01:26:40 by pipe             ###   ########.fr       */
+/*   Updated: 2024/11/04 17:26:46 by dbonilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostrem.h>
+#include <iostream>
 
 int main() {
     PhoneBook phoneBook;
     std::string command;
-
-    while (true) {
-        std::cout << "Enter a command (ADD, SEARCH, EXIT): ";
-        std::getline(std::cin, command);
-
-        if (command == "ADD") {
-            phoneBook.addContact();
-        } else if (command == "SEARCH") {
-            phoneBook.searchContacts();
-        } else if (command == "EXIT") {
-            break;
-        } else {
-            std::cout << "Unrecognized command. Please try again." << std::endl;
-        }
-    }
 
     return 0;
 }
